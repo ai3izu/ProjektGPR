e28c563bf53dfbca7fe1df5e03b2e7379a04dcd7
ai3izu <sebcioo003@gmail.com>
2022-11-12 20:16:58 +0100

    Zmiana 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,12 @@ namespace ProjektGPR
 {
     internal class Program
     {
-        // klasa random gernerujaca losowe liczby dla trybu nr 10
+        // klasa random gernerujaca losowe liczb
         static Random losowaLiczba = new Random();
         static void Main(string[] args)
-        {   
+        {
             // zmiena definujaca wybor trybu przez uzytkownika
-            int wyborTrybu; 
+            int wyborTrybu;
             do
             {
                 Console.WriteLine("\tProjektGRP - wpisz liczbe od 1 - 10 aby wybrac tryb");
@@ -36,69 +37,237 @@ namespace ProjektGPR
                 // warunki, dla kazdego wybranego trybu ( od 1 do 10) bedzie wykonywac sie cos innego
                 if (wyborTrybu == 1)
                 {
-                    
+                    szukanieBinarne();
                 }
                 else if (wyborTrybu == 2)
                 {
-
+                    obliczSilnie();
                 }
-                else if  (wyborTrybu == 3)
+                else if (wyborTrybu == 3)
                 {
-
+                    sortowanieBabelkowe();
                 }
                 else if (wyborTrybu == 4)
                 {
-
+                    czyPalindrom();
                 }
                 else if (wyborTrybu == 5)
                 {
-
+                    //5.Szyfrowanie – szyfr Cezara
                 }
                 else if (wyborTrybu == 6)
                 {
-
+                    // 6.Wyznaczanie wartości wyrażenia zapisanego w odwrotnej notacji polskiej ONP
                 }
                 else if (wyborTrybu == 7)
                 {
-
+                    minAndMax();
                 }
                 else if (wyborTrybu == 8)
                 {
-
+                    Console.WriteLine("siema");
+                    // Mnożenie macierzy 2-wymiarowej.
                 }
                 else if (wyborTrybu == 9)
                 {
-
+                    //9.Transpozycja macierzy 3x3
                 }
                 else if (wyborTrybu == 10)
                 {
-                    
+                    chwilaRelaksu();
                 }
                 if (wyborTrybu != 0)
                 {
-                    Console.Clear();
                     wyborTrybu = czyKontynowac();
                     Console.Clear();
                 }
             } while (wyborTrybu != 0);
         }
         // funkcja ktora pozwala uzytkownikowi kontynuowac gre lub zamknac program
-       static int czyKontynowac()
+        static int czyKontynowac()
         {
-            Console.WriteLine("\t====================================================");
-            Console.WriteLine("\t Czy chesz kontynowac gre????");
-            Console.WriteLine("\t Nacisnij 1 aby kontynuowac albo 2 zeby przerwac gre");
-            Console.WriteLine("\t====================================================");
+            Console.WriteLine("====================================================");
+            Console.WriteLine("Czy chesz kontynowac gre????");
+            Console.WriteLine("Nacisnij 1 aby kontynuowac albo 2 zeby przerwac gre");
+            Console.WriteLine("====================================================");
             int wybor = Convert.ToInt32(Console.ReadLine());
             if (wybor == 1)
             {
-                 return 1;
+                return 1;
             }
             else return 0;
         }
         // funkcja odnajdujaca minimalny i maksymalny element z tablicy liczb podanych przez uzytkownika
-       static void minAndMax()
+        static void minAndMax()
         {
+            Console.Clear();
+            Console.WriteLine("\tWybrano tryb 7 - Min i Max");
+            Console.WriteLine("Podaj ile liczb chcesz wprowadzic: ");
+            int liczbyUzytkownika = Convert.ToInt32(Console.ReadLine());
+            int[] tablicaUzytkownika = new int[liczbyUzytkownika];
+            for (int i = 0; i < tablicaUzytkownika.Length; i++)
+            {
+                int tmp = i + 1;
+                Console.WriteLine($"Podaj {tmp} liczbe");
+                tablicaUzytkownika[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int maksymalnyElement = tablicaUzytkownika.Max();
+            int minimalnyElement = tablicaUzytkownika.Min();
+            Console.WriteLine($"Najwieksza wartosca jest {maksymalnyElement}");
+            Console.WriteLine($"Najmniejsza wartosca jest {minimalnyElement}");
+        }
+        // funkcja sortujaca babelkowo 10 losowych liczb z przedzialu 1,50 wygenerowane funkcja random
+        static void sortowanieBabelkowe()
+        {
+            Console.Clear();
+            Console.WriteLine("\tWybrano tryb 3 - Sortowanie Babelkowe");
+            Console.WriteLine("Oto twoje wylosowane liczby: ");
+            int[] sortTab = new int[10];
+            for (int i = 0; i < sortTab.Length; i++)
+            {
+                sortTab[i] = losowaLiczba.Next(1, 50);
+                Console.Write($"{sortTab[i]}  ");
+            }
+            Console.WriteLine("");
+            Console.Write("Aby posortowac liczby wpisz 1 => ");
+            int czySortowac = Convert.ToInt32(Console.ReadLine());
+            if (czySortowac == 1)
+            {
+                for (int i = 1; i < sortTab.Length; i++)
+                {
+                    for (int j = 1; j < sortTab.Length; j++)
+                    {
+                        if (sortTab[j - 1] > sortTab[j])
+                        {
+                            int tmp = sortTab[j - 1];
+                            sortTab[j - 1] = sortTab[j];
+                            sortTab[j] = tmp;
+                        }
+                    }
+                }
+                for (int i = 0; i < sortTab.Length; i++)
+                {
+                    Console.Write(sortTab[i] + "  ");
+                }
+                Console.WriteLine(" ");
+            }
+            else
+            {
+                Console.WriteLine("Blad nie wpisales wymaganej liczby");
+                Environment.Exit(0);
+            }
+        }
+        static void chwilaRelaksu()
+        {
+            Console.Clear();
+            Console.WriteLine("\tWybrano tryb 10 - Czas Relaksu");
+            Console.WriteLine("!!Wpisz zakres z jakiego przedzialu ma wylosowac liczbe i postaraj sie ja odgadnac!!");
+            int przedzial = Convert.ToInt32(Console.ReadLine());
+            int wylosowanaLiczba = losowaLiczba.Next(1, przedzial + 1);
+            int liczbaWpisana = 0;
+            Console.WriteLine("Sproboj zgadac liczbe :))))");
+            do
+            {
+                liczbaWpisana = Convert.ToInt32(Console.ReadLine());
+                if (liczbaWpisana > wylosowanaLiczba)
+                {
+                    Console.WriteLine("Sproboj mniejsza liczbe");
+                }
+                else if (liczbaWpisana < wylosowanaLiczba)
+                {
+                    Console.WriteLine("Sproboj wieksza liczbe");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Brawo zgadles liczbe!  Liczba to  {wylosowanaLiczba}");
+                }
+            } while (liczbaWpisana != wylosowanaLiczba);
+        }
+        // funkcja sprawdzajaca czy wyraz wpisany przez uzytkownika jest palindromem
+        static bool czyPalindrom()
+        {
+            Console.Clear();
+            Console.WriteLine("\t Wybrano tryb 4 - Palindromy");
+            Console.WriteLine("\t ====================================================================================================");
+            Console.WriteLine("\t przykladowe palindromy do sprawdzenia sos, Anna, sedes, zaraz, kajak, zakaz, owocowo, potop, radar.");
+            Console.WriteLine("\t ====================================================================================================");
+            Console.WriteLine("");
+            Console.WriteLine("Wpisz jakis wyraz aby sprawdzic czy jest palindromem");
+            string slowoUzytkownika = Console.ReadLine();
+            if (String.IsNullOrEmpty(slowoUzytkownika)) { return false; }
+            slowoUzytkownika = slowoUzytkownika.ToLower();
+            int dlWyrazu = slowoUzytkownika.Length;
+            for (int i = 0; i < dlWyrazu  /2 ; i++)
+            {
+                if (slowoUzytkownika[i] != slowoUzytkownika[dlWyrazu - i - 1]) 
+                {
+                    Console.WriteLine("nie jest");
+                    return false;
+                }
+            }
+            Console.WriteLine($"{slowoUzytkownika} jest palindromem!!!");
+            return true;
+        }  
+        // obliczanie silni liczby podanej przez uzytkownika
+        static void obliczSilnie()
+        {
+            Console.Clear();
+            Console.WriteLine("\tWybrano tryb 3 - Obliczanie Silni");
+            Console.WriteLine("Podaj liczbe, z ktorej siline chcesz obliczyc");
+            int liczbaUzytkownika = Convert.ToInt32(Console.ReadLine());
+            int wynik = 1;
+            for (int i = 1; i <= liczbaUzytkownika; i++)
+            {
+                wynik *= i;
+            }
+            
+            Console.Write($"Silnia obliczonej liczby {wynik}  ");
+            Console.WriteLine(" ");
+        }
+        // funkcja ktora korzysta z funkcji zawierajaca algortym wyszukiwania binarnego, prosi uzytkownika o podanie ile chce liczb z danego przedzialu a nastepnie odnajduje szukana
+        static void szukanieBinarne()
+        {
+            Console.Clear();
+            Console.WriteLine("\t Wybrano tryb 1 - Szukanie binarne");
+            Console.WriteLine("Podaj ile liczb chcesz wygenerowac:");
+            int liczbyUzytkownik = Convert.ToInt32(Console.ReadLine());
+            int[] tablicaUzytkownik = new int[liczbyUzytkownik];
+            for (int i = 0; i < tablicaUzytkownik.Length; i++)
+            {
+                tablicaUzytkownik[i] = losowaLiczba.Next(1, 10);
+            }
+            Console.WriteLine("Podaj szukana liczbe");
+            int szukana = Convert.ToInt32(Console.ReadLine());
+            int wynikUzytkownik = szukanieBinarneAlgorytm(tablicaUzytkownik, szukana);
+            if (wynikUzytkownik == -1)
+            {
+                Console.WriteLine("Nie znaleziono szukanej liczby");
+            }
+            else Console.WriteLine($"Szukana znajduje sie pod {wynikUzytkownik} indeksem");
+        }
+        // funkcja wykorzustujaca algortym klasyczny wyszukiwania binarnego
+        static int szukanieBinarneAlgorytm(int[] tablicaLiczb, int szukana)
+        {
+            int lewaStrona = 0;
+            int prawaStrona = tablicaLiczb.Length - 1;
+            while (lewaStrona <= prawaStrona)
+            {
+                int obecnaPozycja = lewaStrona + (prawaStrona - lewaStrona) / 2;
+                if (tablicaLiczb[obecnaPozycja] == szukana)
+                {
+                    return obecnaPozycja;
+                }
+                if (tablicaLiczb[obecnaPozycja] < szukana)
+                {
+                    lewaStrona = obecnaPozycja + 1;
+                }
+                if (tablicaLiczb[obecnaPozycja] > szukana)
+                {
+                    prawaStrona = obecnaPozycja - 1;
+                }
+            }
+            return -1;
 
         }
     }
